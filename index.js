3fce2e4a387d8ccf40c14e1b4e3d82f925a79d5d
Jabed Hossain <hossainjabed3@gmail.com>
2021-04-20 04:26:31 +0600

    developing the server

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express')
 const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectId;
-const port = process.env.PORT || 5555
+const port = process.env.PORT || 4444
 require('dotenv').config()
 
 const app = express()
 
-// https://github.com/Porgramming-Hero-web-course/full-stack-client-zabedbinsiraz
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2ruwh.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 console.log(uri)
@@ -21,31 +21,31 @@ app.use(cors())
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
  
-  const tShirtCollection = client.db("tShirtShop").collection("tShirts");
+  const serviceCollection = client.db("Plumbing-Hero").collection("services");
  
 
 
-  app.get('/tShirts',(req,res) => {
-    tShirtCollection.find()
-    .toArray((err,shirts) => {
-      res.send(shirts)
+  app.get('/services',(req,res) => {
+    serviceCollection.find()
+    .toArray((err,documents) => {
+      res.send(documents)
     })
   })
   
 
-  app.get('/tShirt/:id',(req,res) => {
+  app.get('/service/:id',(req,res) => {
     const id = ObjectId(req.params.id)
-    tShirtCollection.find({_id:id})
-    .toArray((err, shirt) =>{
-      res.send(shirt[0])
+    serviceCollection.find({_id:id})
+    .toArray((err, item) =>{
+      res.send(item[0])
     })
   })
   
 
-  app.post('/addProduct',(req,res)=>{
-    const newShirt = req.body;
+  app.post('/addService',(req,res)=>{
+    const newService = req.body;
     
-    tShirtCollection.insertOne(newShirt)
+    serviceCollection.insertOne(newService)
     .then(result => {
       console.log('inserted one', result.insertedCount)
       // res.send(result.insertedCount > 0)
@@ -55,10 +55,10 @@ client.connect(err => {
 
 
 
-  app.delete('/deleteProduct/:id',(req,res) => {
+  app.delete('/deleteService/:id',(req,res) => {
     const id = ObjectId(req.params.id)
     console.log(id)
-    tShirtCollection.deleteOne({_id:id})
+    serviceCollection.deleteOne({_id:id})
     .then(result => {
          res.send(result.deletedCount > 0)
     })
@@ -68,13 +68,20 @@ client.connect(err => {
 
 client.connect(err => {
 
-  const orderCollection = client.db("tShirtShop").collection("orders");
+  const orderCollection = client.db("Plumbing-Hero").collection("orders");
 
   app.get('/allOrders',(req,res) => {
     orderCollection.find({buyerEmail: req.query.email})
-    .toArray((err,products) => {
-       console.log(products)
-       res.send(products)
+    .toArray((err,orders) => {
+      //  console.log(orders)
+       res.send(orders)
+    })
+  })
+  app.get('/orders',(req,res) => {
+    orderCollection.find()
+    .toArray((err,allOrders) => {
+      //  console.log(allOrders)
+       res.send(allOrders)
     })
   })
 
@@ -82,6 +89,29 @@ client.connect(err => {
     const newOrder = req.body;
     
     orderCollection.insertOne(newOrder)
+    .then(result => {
+      console.log('inserted one', result.insertedCount)
+      res.send(result.insertedCount > 0)
+    })
+  }) 
+});
+
+client.connect(err => {
+
+  const reviewCollection = client.db("Plumbing-Hero").collection("reviews");
+
+  app.get('/allReviews',(req,res) => {
+    reviewCollection.find()
+    .toArray((err,reviews) => {
+      //  console.log(reviews)
+       res.send(reviews)
+    })
+  })
+
+  app.post('/addReview',(req,res)=>{
+    const newReview = req.body;
+    
+    reviewCollection.insertOne(newReview)
     .then(result => {
       console.log('inserted one', result.insertedCount)
       res.send(result.insertedCount > 0)
